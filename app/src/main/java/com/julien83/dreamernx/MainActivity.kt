b8595754c41af5b68d4bdd93429d15ac707a48de
julien83 <julienjunique@gmail.com>
2023-01-18 07:10:20 +0100

    Try/Catch dédié a l'ouverture du socket et son en cas d'erreur ajout du status dans l'IHM et son rafraichisement

--- app/src/main/java/com/julien83/dreamernx/MainActivity.kt
+++ app/src/main/java/com/julien83/dreamernx/MainActivity.kt
@@ -13,6 +13,7 @@ import java.net.InetAddress
 import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var StatusView: TextView
     private lateinit var BuseTempView: TextView
     private lateinit var BedTempView: TextView
     private lateinit var PrintProgessBar: ProgressBar
@@ -30,11 +31,15 @@ class MainActivity : AppCompatActivity() {
         PrintProgessBar = findViewById(R.id.progressBar)
         ButtonConnect = findViewById(R.id.buttonCONNECT)
         ButtonStop = findViewById(R.id.buttonSTOP)
+        StatusView = findViewById(R.id.textStatus)
 
 
         ButtonConnect.setOnClickListener{
+            //recupération des valeur de l'IP et du Port
             ipEditText = findViewById(R.id.editIP)
             portEditText = findViewById(R.id.editPORT)
+            //Maj du status
+            StatusView.text = "Connecting"
             // Send message to server in a background thread
             runthead = true
             SendMessageTask().execute()
@@ -50,81 +55,105 @@ class MainActivity : AppCompatActivity() {
         override fun doInBackground(vararg params: Void): List<String>
         {
             try {
+
+                //recupération de l'IP et du Port de IHM
                 val serverAddress = InetAddress.getByName(ipEditText.text.toString())
                 val serverPort = portEditText.text.toString().toInt()
-
-                //val soc = Socket("192.168.1.79", 8899)
+                //Overture du socket
                 val soc = Socket(serverAddress, serverPort)
+                try {
+                    //Ouverture des flux d'entrée et de sortie du socket
+                    val dataout = DataOutputStream(soc.getOutputStream())
+                    val inputStream = BufferedReader(InputStreamReader(soc.getInputStream()))
 
-                val dataout = DataOutputStream(soc.getOutputStream())
-                val inputStream = BufferedReader(InputStreamReader(soc.getInputStream()))
+                    //boucle d'envoie des trame de demande d'information
+                    while(runthead){
+                        //Connexion a la machine
+                        dataout.writeBytes("~M601 S1\r\n")
+                        dataout.flush()
+                        var cpt = 0
+                        while (!inputStream.ready()|| cpt< 3)
+                        {
+                            Thread.sleep(10)
+                            cpt++
+                        }
+                        val response1 = inputStream.readLine()
+                        val response2 = inputStream.readLine()
+                        val response3 = inputStream.readLine()
 
-                while(runthead){
-                    //Connexion a la machine
-                    dataout.writeBytes("~M601 S1\r\n")
-                    dataout.flush()
-                    val response1 = inputStream.readLine()
-                    val response2 = inputStream.readLine()
-                    val response3 = inputStream.readLine()
+                        //Avancement 3DPrint
+                        dataout.writeBytes("~M27\r\n")
+                        dataout.flush()
+                        val response11 = inputStream.readLine()
+                        val response12 = inputStream.readLine()
+                        val response13 = inputStream.readLine()
 
-                    //Avancement 3DPrint
-                    dataout.writeBytes("~M27\r\n")
-                    dataout.flush()
-                    val response11 = inputStream.readLine()
-                    val response12 = inputStream.readLine()
-                    val response13 = inputStream.readLine()
+                        //Temperature machine
+                        dataout.writeBytes("~M105\r\n")
+                        dataout.flush()
+                        val response21 = inputStream.readLine()
+                        val response22 = inputStream.readLine()
+                        val response23 = inputStream.readLine()
 
-                    //Temperature machine
-                    dataout.writeBytes("~M105\r\n")
-                    dataout.flush()
-                    val response21 = inputStream.readLine()
-                    val response22 = inputStream.readLine()
-                    val response23 = inputStream.readLine()
-
-                    // Demande Statut
-                    /*dataout.writeBytes("~M119\r\n")
-                    dataout.flush()
-                    val response31 = inputStream.readLine()
-                    val response32 = inputStream.readLine()
-                    val response33 = inputStream.readLine()
-                    val response34 = inputStream.readLine()*/
+                        // Demande Statut
+                        /*dataout.writeBytes("~M119\r\n")
+                        dataout.flush()
+                        val response31 = inputStream.readLine()
+                        val response32 = inputStream.readLine()
+                        val response33 = inputStream.readLine()
+                        val response34 = inputStream.readLine()*/
 
 
-                    var printingList = response12.split(" ", "/")
-                    var tempList = response22.split(":", "/")
+                        var printingList = response12.split(" ", "/")
+                        var tempList = response22.split(":", "/")
 
 
-                    var listRet = listOf<String>(
-                        tempList.get(1),
-                        tempList.get(3),
-                        printingList.get(3),
-                        printingList.get(4)
-                    )
+                        var listRet = listOf<String>(
+                            tempList.get(1),
+                            tempList.get(3),
+                            printingList.get(3),
+                            printingList.get(4)
+                        )
 
-                    publishProgress(listRet)
-                    Thread.sleep(1000)
+                        publishProgress(listRet)
+                        Thread.sleep(1000)
+                    }
+                    dataout.close()
+                    soc.close()
+                    var listOk = listOf<String>("Ok","Finish")
+                    return listOk
                 }
-                dataout.close()
-                soc.close()
-                var listOk = listOf<String>("Ok","Finish")
-                return listOk
+                catch (e: Exception) {
+                    e.printStackTrace()
+                    var listErr = listOf<String>("Error", "Error: ${e.message}")
+                    return listErr
+
+                }
             }
             catch (e: Exception) {
                 e.printStackTrace()
-                var listErr = listOf<String>("Error", "Error: ${e.message}")
+                //var listErr = listOf<String>("Error", "Error: ${e.message}")
+                var listErr = listOf<String>("Error", "3DPrinter No Found")
                 return listErr
-
             }
         }
 
         override fun onPostExecute(listRet: List<String>) {
 
-                //Toast.makeText(this, listRet.get(1), Toast.LENGTH_SHORT).show();
-                Toast.makeText(this@MainActivity,listRet.get(1),Toast.LENGTH_SHORT).show()
+            StatusView.text = "OFF"
+            if(listRet.get(0).equals("Error"))
+            {
+                val toneGen1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+                toneGen1.startTone(ToneGenerator.TONE_CDMA_ALERT_NETWORK_LITE, 150)
+            }
+
+            Toast.makeText(this@MainActivity,listRet.get(1),Toast.LENGTH_SHORT).show()
+
         }
 
         override fun onProgressUpdate(vararg values: List<String>) {
             super.onProgressUpdate(*values)
+            StatusView.text = "Connected"
             BuseTempView.text = values.get(0).get(0)+"°C"
             BedTempView.text = values.get(0).get(1)+"°C"
             val printingBytes = values.get(0).get(2).toInt()
